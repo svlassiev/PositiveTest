66f66336d26686e51791ebecff51a08af3b91b69
svlassiev <svlassiev@gmail.com>
2015-05-06 17:11:39 +0300

    Handle import declarations.

--- src/com/positive/CompileSubsetDetector.java
+++ src/com/positive/CompileSubsetDetector.java
@@ -4,58 +4,111 @@ import japa.parser.JavaParser;
 import japa.parser.ParseException;
 import japa.parser.ast.CompilationUnit;
 import japa.parser.ast.ImportDeclaration;
+import japa.parser.ast.body.FieldDeclaration;
+import japa.parser.ast.body.InitializerDeclaration;
 import japa.parser.ast.body.MethodDeclaration;
-import japa.parser.ast.body.TypeDeclaration;
-import japa.parser.ast.expr.NameExpr;
 import japa.parser.ast.visitor.VoidVisitorAdapter;
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.filefilter.AbstractFileFilter;
+import org.apache.commons.io.filefilter.IOFileFilter;
+import org.apache.commons.io.filefilter.TrueFileFilter;
 
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.HashMap;
-import java.util.Map;
+import java.nio.file.StandardCopyOption;
+import java.util.*;
 
 /**
  * Created by serg on 06.05.2015.
  */
 public class CompileSubsetDetector {
 
-    private Map<Class, File> compileSubset = new HashMap<>();
+    private final File root;
+    private Map<File, File> compileSubset = new HashMap<>();
+    private Queue<File> compilationFiles = new LinkedList<>();
+
+    public CompileSubsetDetector() throws IOException {
+        root = new File(FileUtils.getTempDirectory(), "PositiveTechnologiesTest" + System.currentTimeMillis());
+        if (!root.mkdirs()) {
+            throw new IOException("Unable to create root directory for compilation subset: " + root);
+        };
+    }
 
     public File getCompileSubset(File compilationFile, Path project) throws IOException, ParseException {
 
-        try (FileInputStream in = new FileInputStream(compilationFile)) {
-            CompilationUnit unit = JavaParser.parse(in);
-            if (null != unit.getImports()) {
-                unit.getImports().forEach(importDeclaration -> {
-                    if (!importDeclaration.isAsterisk()) {
-                        //TBD
-                    }
-                    System.out.print(importDeclaration);
-                });
+        compilationFiles.add(compilationFile);
+        while (!compilationFiles.isEmpty()) {
+            try (FileInputStream in = new FileInputStream(compilationFiles.poll())) {
+                CompilationUnit unit = JavaParser.parse(in);
+                new MethodVisitor().visit(unit, project);
             }
-            System.out.println(unit.getPackage());
-            // visit and print the methods names
-            new MethodVisitor().visit(unit, null);
         }
-        return null;
+        return root;
     }
 
     /**
      * Simple visitor implementation for visiting MethodDeclaration nodes.
      */
-    private static class MethodVisitor extends VoidVisitorAdapter {
+    private class MethodVisitor extends VoidVisitorAdapter {
 
         @Override
         public void visit(MethodDeclaration method, Object arg) {
-            // here you can access the attributes of the method.
-            // this method will be called for all methods in this
-            // CompilationUnit, including inner class methods
             System.out.println(method.getName() + " type: " + method.getType());
-            if (null != method.getThrows()) {
-                method.getThrows().forEach(throwsName -> System.out.println("throws " + throwsName));
+        }
+
+        @Override
+        public void visit(FieldDeclaration n, Object arg) {
+            System.out.println("Field declaration: " + n);
+        }
+
+        @Override
+        public void visit(ImportDeclaration n, Object arg) {
+            if (arg instanceof Path) {
+                Path project = (Path) arg;
+                String importName = n.getName().toString().replace(".", File.separator) +
+                        (n.isAsterisk() ? "" : ".java");
+                IOFileFilter filter = new AbstractFileFilter() {
+                    @Override
+                    public boolean accept(File file) {
+                        return file.getAbsolutePath().endsWith(importName);
+                    }
+                };
+                Collection<File> files = FileUtils.listFiles(project.toFile(), filter, TrueFileFilter.INSTANCE);
+                if (files.size() > 1) {
+                    throw new IllegalArgumentException("There are several possible ways to solve the task with these " +
+                            "parameters");
+                }
+                files.forEach(file -> {
+                    if (!compileSubset.containsKey(file)) {
+                        compilationFiles.add(file);
+                        try {
+                            File copyDir = n.isAsterisk() ? new File(root, importName) :
+                                    new File(root, importName).getParentFile();
+                            copyDir.mkdirs();
+                            if (n.isAsterisk()) {
+                                compileSubset.put(file, copyDir);
+                            } else {
+                                File copy = new File(copyDir, file.getName());
+                                Files.copy(file.toPath(), copy.toPath(), StandardCopyOption.COPY_ATTRIBUTES);
+                                compileSubset.put(file, copy);
+                            }
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                            throw new RuntimeException("Unable to copy file to root directory: " + e.getMessage());
+                        }
+                    }
+                });
             }
         }
+
+        @Override
+        public void visit(InitializerDeclaration n, Object arg) {
+            System.out.println("Initializer declaration: " + n);
+        }
     }
+
+
 }
--- src/com/positive/Main.java
+++ src/com/positive/Main.java
@@ -1,15 +1,11 @@
 package com.positive;
 
-import japa.parser.ParseException;
-
 import java.io.File;
-import java.io.IOException;
 import java.nio.file.Path;
 
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println(args.length);
         if (args.length != 2) {
             System.out.println("How to use this app");
         } else {
@@ -19,10 +15,10 @@ public class Main {
                 CompileSubsetDetector subsetDetector = new CompileSubsetDetector();
                 File compilationFolder = subsetDetector.getCompileSubset(unit, project);
                 System.out.println(compilationFolder);
-            } catch (IOException | ParseException e) {
+            } catch (Throwable t) {
                 System.err.println("Unable to find compilation subset for file " + unit + " and project " +
-                        project + ". " + e.getMessage());
-                e.printStackTrace();
+                        project + ". " + t.getMessage());
+                t.printStackTrace();
             }
         }
     }
